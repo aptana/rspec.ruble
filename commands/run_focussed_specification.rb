61e0e7fad0863a726a6b9d1d639fc4fbb7a768e9
Allen Yeung <ayeung@aptana.com>
2010-04-08 18:03:08 -0700

    Fixed "Run Examples" and "Run Single Example" commands

--- commands/run_focussed_specification.rb
+++ commands/run_focussed_specification.rb
@@ -1,20 +1,45 @@
 require 'ruble'
+require 'ruble/terminal'
+require 'ruble/ui'
+require 'ruble/editor'
 
 command 'Run Single Example' do |cmd|
   cmd.key_binding = 'M1+M2+R'
   cmd.scope = 'source.ruby.rspec'
   cmd.output = :show_as_html
-  cmd.input = :document, :line
-  cmd.invoke =<<-EOF
-#!/usr/bin/env sh
-cat <<'RUBYEOF' > /tmp/textmate-command-$$.rb
+  cmd.input = :selection, :line
+  cmd.invoke do |context| 
 
-require ENV['TM_BUNDLE_SUPPORT'] + "/lib/spec/mate"
-Spec::Mate::Runner.new.run_focussed STDOUT
+    line = start_line = ENV["TM_CARET_LINE_NUMBER"].to_i - 1
 
-RUBYEOF
+    if context.input =~ /^[\s]*it[\s]+["']/
+      example_name = context.input.scan(/^\s*it\s+["'].*["']/).first.scan(/["'].*["']/).first
+      Ruble::Terminal.open("spec #{ENV["TM_FILENAME"]} -e #{example_name}", ENV["TM_DIRECTORY"] )
+    elsif Ruble::Editor.active.styled_text.line_count.to_i == 1
+      Ruble::UI.alert(:info, '', "There are no examples to run.")
+    else
+      while (Ruble::Editor.active.styled_text.line(line).to_s =~ /^[\s]*it[\s]+["']/).nil?
+        
+        if line <= start_line
+          line -= 1
+        else
+          line += 1
+        end
 
-${TM_RUBY:-ruby} /tmp/textmate-command-$$.rb; exit_status=$?; rm /tmp/textmate-command-$$.rb; exit $exit_status
-
-EOF
+        if line < 0
+          line = start_line + 1
+          break if Ruble::Editor.active.styled_text.line_count.to_i <= start_line + 1
+        elsif line > Ruble::Editor.active.styled_text.line_count.to_i - 1
+          break
+        end        
+      end
+      
+      if line >= Ruble::Editor.active.styled_text.line_count.to_i - 1
+        Ruble::UI.alert(:info, '', "There are no examples to run")
+      else
+        example_name = Ruble::Editor.active.styled_text.line(line).scan(/^\s*it\s+["'].*["']/).first.scan(/["'].*["']/).first
+        Ruble::Terminal.open("spec #{ENV["TM_FILENAME"]} -e #{example_name}", ENV["TM_DIRECTORY"] )
+      end
+    end
+  end  
 end
--- commands/run_specifications_-_normal.rb
+++ commands/run_specifications_-_normal.rb
@@ -1,19 +1,9 @@
 require 'ruble'
+require 'ruble/terminal'
 
 command 'Run Examples' do |cmd|
   cmd.key_binding = 'M1+R'
   cmd.scope = 'source.ruby.rspec'
-  cmd.output = :show_as_html
   cmd.input = :document, :document
-  cmd.invoke =<<-EOF
-#!/usr/bin/env sh
-cat <<'RUBYEOF' > /tmp/textmate-command-$$.rb
-
-require ENV['TM_BUNDLE_SUPPORT'] + "/lib/spec/mate"
-Spec::Mate::Runner.new.run_file STDOUT
-
-RUBYEOF
-
-${TM_RUBY:-ruby} /tmp/textmate-command-$$.rb; exit_status=$?; rm /tmp/textmate-command-$$.rb; exit $exit_status
-EOF
+  cmd.invoke {|context| Ruble::Terminal.open("spec #{ENV["TM_FILENAME"]}", ENV["TM_DIRECTORY"]) }
 end
