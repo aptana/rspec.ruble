c1993276ff6c076ba9c97f5a2badcbb40ea3d889
Ingo <ingo@muschenetz.com>
2010-12-06 08:51:07 -0800

    Related fix for issue s [#1400]. We now are using the save trigger event to fire the command in question.

--- commands/save_spec_and_remember.rb
+++ commands/save_spec_and_remember.rb
@@ -1,13 +1,12 @@
 require 'ruble'
 
 command 'Save Example file and remember' do |cmd|
-  cmd.key_binding = 'M1+S'
   cmd.scope = 'source.ruby.rspec'
+  cmd.trigger = :execution_listener, "org.eclipse.ui.file.save" # fires for save events only
   cmd.output = :none
   cmd.input = :none
   cmd.invoke do
     require ENV['TM_BUNDLE_SUPPORT'] + "/lib/spec/mate"
-    
     Spec::Mate::Runner.new.save_as_last_remembered_file ENV['TM_FILEPATH']
   end
   
